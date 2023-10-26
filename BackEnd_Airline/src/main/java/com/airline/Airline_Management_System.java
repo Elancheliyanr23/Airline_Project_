37240e5719c5e36fec29edf7994bdcf3842eda3c
Elancheliyan R <138276701+Elancheliyanr23@users.noreply.github.com>
2023-10-26 12:24:21 +0530

    Add files via upload

---
+++ BackEnd_Airline/src/main/java/com/airline/Airline_Management_System.java
@@ -0,0 +1,15 @@
+package com.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airline_Management_System {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Airline_Management_System.class, args);
+		System.out.println("AirLine_Running");
+	}
+
+}
+ 
